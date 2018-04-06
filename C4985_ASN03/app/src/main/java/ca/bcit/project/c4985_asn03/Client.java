01dd3abf09f1795b8c763fcf554694a5b7a2a778
wozenwho <wilson.huu@gmail.com>
2018-04-05 23:39:48 -0700

    Updated Client class with headers

--- C4985_ASN03/app/src/main/java/ca/bcit/project/c4985_asn03/Client.java
+++ C4985_ASN03/app/src/main/java/ca/bcit/project/c4985_asn03/Client.java
@@ -18,9 +18,29 @@ import java.net.*;
 
 
 /**
- * Created by BlackberryCosmos on 2018-03-27.
+ * CLASS:       Client
+ *
+ * FUNCTIONS:   Client()
+ *              boolean connect(String ipAddr)
+ *              boolean send(String[] coordinates)
+ *              boolean closeSocket()
+ *              boolean isConnected()
+ *
+ * DATE:        Apr. 29, 2018
+ *
+ * REVISIONS:
+ *
+ * DESIGNER:    Wilson Hu
+ *
+ * PROGRAMMER:  Wilson Hu
+ *
+ * NOTES:
+ * The Client class holds the connection methods used to connect the app to
+ * the remote server.
+ *
+ * This class contains the socket and stream variables used to write data
+ * to the socket, which is to be read by the server.
  */
-
 public class Client {
 
     public static final int PORT_NO = 42069;
@@ -32,20 +52,58 @@ public class Client {
     Integer android_id;
     boolean connected = false;
 
+
+    /**
+     * FUNCTION:    Client constructor
+     *
+     * DATE:        Apr. 29, 2018
+     *
+     * REVISIONS:
+     *
+     * DESIGNER:    Wilson Hu
+     *
+     * PROGRAMMER:  Wilson Hu
+     *
+     * INTERFACE:   Client()
+     *
+     * RETURNS:
+     *
+     * NOTES:
+     * This function is the client class' constructor.
+     * It sets the android_id variable to a hashed value of
+     * the device's unique AndroidID. The absolute value of the
+     * hash is used to prevent sending negative ID values.
+     *
+     */
     public Client()
     {
-//        try
-//        {
-//            //opToServer = socket.getOutputStream();
-//            //out = new DataOutputStream(opToServer);
-//        }
-//        catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-        android_id = MainActivity.androidID.hashCode();
+        android_id = Math.abs(MainActivity.androidID.hashCode());
     }
 
+
+    /**
+     * FUNCTION:    connect
+     *
+     * DATE:        Apr. 29, 2018
+     *
+     * REVISIONS:
+     *
+     * DESIGNER:    Wilson Hu
+     *
+     * PROGRAMMER:  Wilson Hu
+     *
+     * INTERFACE:   connect(String ipAddr)
+     *                  String ipAddr: the IP address String of the server
+     *
+     * RETURNS:     boolean - indicates if close socket operation succeeds or fails
+     *
+     * NOTES:
+     * This function is used to establish a TCP connection between the client app and
+     * the server program.
+     *
+     * This function creates the socket as well as the datastreams used to transmit
+     * data through the socket.
+     */
     public boolean connect(String ipAddr)
     {
         try
@@ -63,6 +121,30 @@ public class Client {
         }
     }
 
+    /**
+     * FUNCTION:    send
+     *
+     * DATE:        Apr. 29, 2018
+     *
+     * REVISIONS:
+     *
+     * DESIGNER:    Wilson Hu
+     *
+     * PROGRAMMER:  Wilson Hu
+     *
+     * INTERFACE:   boolean send(String[] coordinates)
+     *
+     * RETURNS:     boolean - indicating if the send operation succeeds or fails
+     *                      - returns false if the function throws an IOException
+     *
+     * NOTES:
+     * This function is used to send the device's coordinates to the server.
+     * It accepts coordinates as a String array where index 0 stores the Lat
+     * and index 1 stores the Lng.
+     *
+     * The unique hashed AndroidID is prepended to the output String, and
+     * each element (ID, Lat, Lng) is separated by a '/'.
+     */
     public boolean send(String[] coordinates)
     {
         try
@@ -79,6 +161,25 @@ public class Client {
         }
     }
 
+    /**
+     * FUNCTION:    closeSocket
+     *
+     * DATE:        Apr. 29, 2018
+     *
+     * REVISIONS:
+     *
+     * DESIGNER:    Wilson Hu
+     *
+     * PROGRAMMER:  Wilson Hu
+     *
+     * INTERFACE:
+     *
+     * RETURNS:     boolean - indicates if close socket operation succeeds or fails
+     *
+     * NOTES:
+     * This function is used to close the socket when the connection terminates. The
+     * app should call this when the user wants to disconnect from the server.
+     */
     public boolean closeSocket()
     {
         try
@@ -95,6 +196,25 @@ public class Client {
 
     }
 
+
+    /**
+     * FUNCTION:    isConnected
+     *
+     * DATE:        Apr. 29, 2018
+     *
+     * REVISIONS:
+     *
+     * DESIGNER:    Wilson Hu
+     *
+     * PROGRAMMER:  Wilson Hu
+     *
+     * INTERFACE:   boolean isConnected()
+     *
+     * RETURNS:     boolean - indicating if the client app is connected.
+     *
+     * NOTES:
+     * This function returns true if the client app is connected.
+     */
     public boolean isConnected()
     {
         return connected;
